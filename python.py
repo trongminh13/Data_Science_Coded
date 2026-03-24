20010fc256a7921b9a8fc4c49f0cf8d1cdc2c924
Minh2255 <80587574+trongminh13@users.noreply.github.com>
2026-03-24 16:24:16 +0700

    fix code already

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
-c = "fg"
+a = {"day": "Monday", "temperature": 32}
+print(a.keys())
+print(a.values())
+print(a.items())
+
+# total = 0
+# for i in range(101):
+#     total += i
+
+total = sum([i for i in range(101) if i % 2 == 1])
+print(total)
